0a37331206b10076a5d24a28d56215f625ed4a67
Judaseec <jdsernae@hotmail.com>
2016-07-25 16:21:01 -0500

    Scroll parallax
    Creación del Scroll parallax para el background

---
+++ Assets/Game/Scripts/Scroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+
+	/*
+	 * @brief Variable de velocidad en la que se desplaza el fondo_3 de la pantalla
+	 */
+	public float velocity = 0f;
+	//private Renderer renderer = GetComponent<Renderer>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (Time.time * velocity, 0);
+	}
+}
