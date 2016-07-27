758d443fa1eb8efabec7987269a4a1b532cd985d
dmhoyosc <dmhoyosc@outlook.com>
2016-07-27 16:54:40 -0500

    Incorporación personaje de prueba

---
+++ Assets/Game/Scripts/ControladorPersonaje.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControladorPersonaje : MonoBehaviour {
+
+	public float fuerzaSalto = 100f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, fuerzaSalto));
+		}
+	}
+}
