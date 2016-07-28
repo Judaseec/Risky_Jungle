66e97c172a8cfa274f1b29aff932d7463e661ddd
Judaseec <jdsernae@hotmail.com>
2016-07-28 16:30:02 -0500

    Animaciones implementadas

--- Assets/Game/Scripts/ControladorPersonaje.cs
+++ Assets/Game/Scripts/ControladorPersonaje.cs
@@ -3,27 +3,52 @@ using System.Collections;
 
 public class ControladorPersonaje : MonoBehaviour {
 
-	public float jumpForce = 300f;
+	public float jumpForce = 1500f;
 
-	public bool isGrounded = true;
+	private bool isGrounded = true;
 	public Transform groundChecker;
-	private float radiusChecker = 0.07f;
+	private float radiusChecker = 0.03f;
 	public LayerMask floorMask;
+
+	private Animator animator;
+	public bool run = false;
+	public float velocity = 1f;
+
+	void Awake() {
+		animator = GetComponent<Animator>();
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 
 	void FixedUpdate(){
+
+		GetComponent<Rigidbody> ().AddForce (Physics.gravity * GetComponent<Rigidbody> ().mass);
+
 		Collider[] hitColliders = Physics.OverlapSphere (groundChecker.position, radiusChecker, floorMask);
 		isGrounded = (hitColliders.Length > 0);
+		//animator.SetBool ("isGrounded", isGrounded);
+
+		if (run) {
+			GetComponent<Rigidbody>().velocity = new Vector3(velocity, GetComponent<Rigidbody>().velocity.y, GetComponent<Rigidbody>().velocity.z);
+
+		}
+		animator.SetFloat ("velX", GetComponent<Rigidbody>().velocity.x);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown("up")) {
+		if (isGrounded && Input.GetKeyDown(KeyCode.UpArrow)) {
 			GetComponent<Rigidbody>().AddForce(new Vector2(0, jumpForce));
+			GetComponent<Animator> ().Play("jump");
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			run = true;
+		} else {
+			run = false;
 		}
 	}
 }
