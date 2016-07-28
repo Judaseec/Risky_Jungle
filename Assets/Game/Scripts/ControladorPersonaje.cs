10b8ae0098e8f38f218d64d1cfc8f3972ab4f275
Judaseec <jdsernae@hotmail.com>
2016-07-28 11:12:21 -0500

    Implementacion Animacion Idle

--- Assets/Game/Scripts/ControladorPersonaje.cs
+++ Assets/Game/Scripts/ControladorPersonaje.cs
@@ -3,17 +3,26 @@ using System.Collections;
 
 public class ControladorPersonaje : MonoBehaviour {
 
-	public float fuerzaSalto = 100f;
+	public float jumpForce = 300f;
+
+	public bool isGrounded = true;
+	public Transform groundChecker;
+	private float radiusChecker = 0.07f;
+	public LayerMask floorMask;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+	void fixedUpdate(){
+		isGrounded = Physics2D.OverlapCircle (groundChecker.position, radiusChecker, floorMask);
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, fuerzaSalto));
+		if (Input.GetKeyDown("up")) {
+			GetComponent<Rigidbody>().AddForce(new Vector2(0, jumpForce));
 		}
 	}
 }
---
+++ Assets/Game/Scripts/Follow_Character.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow_Character : MonoBehaviour {
+
+	public Transform character;
+	public float separate = 3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (character.position.x + separate, transform.position.y, transform.position.z);
+	}
+}
