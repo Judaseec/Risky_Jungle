30439534db877fb32b2ea4157e3089a0c6ed830e
dmhoyosc <dmhoyosc@outlook.com>
2016-08-02 12:31:23 -0500

    Incorporación del oso y planificación de la segunda iteración
    Creación del prefab del oso, collider de colisión y trigger para decidir
    cuando atacar.
    Primera versión del documento de planificación de la segunda iteración.

--- Assets/Game/Scripts/Bear_Controller.cs
+++ Assets/Game/Scripts/Bear_Controller.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class Bear_Controller : MonoBehaviour {
 
+
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//Debug.Log (GameObject.Find("trigger").GetComponent<Trigger_Controller>().inside);
+		if (GameObject.Find("trigger").GetComponent<Trigger_Controller>().inside) {
+			GetComponent<Animator> ().Play("atacar");
+		}
 	}
 }
---
+++ Assets/Game/Scripts/Trigger_Controller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger_Controller : MonoBehaviour {
+
+	public bool inside = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider hit){
+		//Si el objeto que entra al trigger tiene el tag principal...
+		if (hit.gameObject.tag == "Principal") {
+			inside=true;
+		}
+	}
+
+	void OnTriggerExit(Collider hit){
+		//Si el objeto que sale del trigger tiene el tag principal...
+		if (hit.gameObject.tag == "Principal") {
+			inside=false;
+		}
+	}
+}
