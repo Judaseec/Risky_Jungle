1ea177c207e3f94ccf4ba957ca7fa737d2f0bbfd
dmhoyosc <dmhoyosc@outlook.com>
2016-08-16 12:16:41 -0500

    Implementación clases para serialización y el botón continuar desaparece si no hay partida iniciada

--- Assets/Game/Scripts/Main_Menu.cs
+++ Assets/Game/Scripts/Main_Menu.cs
@@ -1,8 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Assets.Scripts.com.ethereal.util;
 
 public class Main_Menu : MonoBehaviour {
+
+	private GameObject continuebtn; 
+	
 	public void NewGame (){
 		SceneHandler.LoadScene("Tutorial");
+	}
+
+	void Start(){
+		continuebtn = Util.GetChildByName (this.gameObject, "Continue");
+	}
+
+	void Update(){
+		if (Save.savedGame.level == 0 && continuebtn.activeInHierarchy) {
+			continuebtn.SetActive (false);
+		} else if (Save.savedGame.level > 0 && !continuebtn.activeInHierarchy) {
+			continuebtn.SetActive (true);
+		}
 	}
 }
---
+++ Assets/Game/Scripts/Save.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class Save {
+
+	public static Game savedGame = new Game ();
+
+	public static void SaveGame(){
+		savedGame = Game.current;
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (Application.persistentDataPath + "/savedGame.eth");
+		bf.Serialize (file, Save.savedGame);
+		file.Close();
+	}
+
+	public static void LoadGame(){
+		if(File.Exists(Application.persistentDataPath + "/savedGame.eth")){
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/savedGame.eth", FileMode.Open);
+			Save.savedGame = (Game)bf.Deserialize(file);
+			file.Close();
+		}
+	}
+}
