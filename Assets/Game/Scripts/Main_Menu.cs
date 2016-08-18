4ddc9cbb771f19d34aca15dd42a57c3609c8507f
dmhoyosc <dmhoyosc@outlook.com>
2016-08-18 14:40:36 -0500

    Creación de la ventana modal de configuración

--- Assets/Game/Scripts/Main_Menu.cs
+++ Assets/Game/Scripts/Main_Menu.cs
@@ -4,23 +4,46 @@ using Assets.Scripts.com.ethereal.util;
 
 public class Main_Menu : MonoBehaviour {
 
-	private GameObject continuebtn; 
-	
-	public void NewGame (){
-		SceneHandler.LoadScene("Tutorial");
-	}
+	private GameObject continuebtn;
+	private GameObject configPanel;
+
+	private bool isConfEnable;
 
 	void Start(){
 		continuebtn = Util.GetChildByName (this.gameObject, "Continue");
+		configPanel = Util.GetChildByName (this.gameObject, "ConfigPanel");
+		isConfEnable = false;
+		configPanel.SetActive (isConfEnable);
+
+		//Activar los idiomas (EthLang.ActiveLangs)
+
 		string aux = Eth.EncodeBase64 ("asdasdasd");
 		Debug.Log (aux);
 	}
 
 	void Update(){
+
 		if (Save.savedGame.level == 0 && continuebtn.activeInHierarchy) {
 			continuebtn.SetActive (false);
 		} else if (Save.savedGame.level > 0 && !continuebtn.activeInHierarchy) {
-			continuebtn.SetActive (true);
+			Eth.SetVisibleGameObject(continuebtn, false);
 		}
+	}
+
+	public void NewGame (){
+		SceneHandler.LoadScene("Tutorial", this.gameObject);
+	}
+
+	public void Conf (){
+		isConfEnable = !isConfEnable;
+		configPanel.SetActive (isConfEnable);
+	}
+
+	public void Esp (){
+		EthLang.LangAct = "es";
+	}
+
+	public void Eng (){
+		EthLang.LangAct = "en";
 	}
 }
